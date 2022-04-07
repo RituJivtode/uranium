54f9f7ccf3aa1d78083db5783c4e77e7d0d2be32
Ritu Jivtode <ritujivtode13@gmail.com>
2022-04-08 01:37:53 +0530

    my 7th April Assihnment.

--- src/routes/route.js
+++ src/routes/route.js
@@ -9,12 +9,95 @@ router.get('/user-profile/:abcd', function(req, res) {
     res.send('dummy response')
 })
 
-router.get('/test-me', function (req, res) {
+router.get('/test-me', function(req, res) {
     console.log('------------------')
     console.log(req)
     console.log('------------------')
     console.log('These are the request query parameters: ', req.query)
     res.send('My first ever api!')
+});
+
+//7 April Assignment
+// 1 Problem
+
+
+router.get('/movies', function(req, res) {
+    const arr = ['muvi1', 'muvi2', 'muvi3', 'muvi4', 'muvi5']
+    res.send(arr)
+});
+
+// 2 Problem
+
+// router.get('/movies/:indexNumber', function(req, res) {
+//     const arr = ['muvi1', 'muvi2', 'muvi3', 'muvi4', 'muvi5']
+//     let data = req.params.indexNumber;
+//     res.send(arr[data])
+// });
+
+// 2 & 3 problem
+router.get('/movies/:indexNumber', function(req, res) {
+    function getMovies() {
+        const arr = ['muvi1', 'muvi2', 'muvi3', 'muvi4', 'muvi5']
+        let data = req.params.indexNumber;
+        if (arr.length >= data) {
+            return arr[data]
+        } else if (data > arr.length)
+            return "Please enter valid number."
+    }
+    res.send(getMovies())
+});
+
+
+// 4 problem
+// router.get('/films/', function(req, res) {
+
+//     let film = [{
+//         'id': 1,
+//         'name': 'The Shining'
+//     }, {
+//         'id': 2,
+//         'name': 'Incendies'
+//     }, {
+//         'id': 3,
+//         'name': 'Rang de Basanti'
+//     }, {
+//         'id': 4,
+//         'name': 'Finding Nemo'
+//     }]
+
+//     res.send(film)
+
+
+// });
+
+// 5 problem
+router.get('/films/:filmId', function(req, res) {
+    function printMovies() {
+        let film = [{
+            'id': 1,
+            'name': 'The Shining'
+        }, {
+            'id': 2,
+            'name': 'Incendies'
+        }, {
+            'id': 3,
+            'name': 'Rang de Basanti'
+        }, {
+            'id': 4,
+            'name': 'Finding Nemo'
+        }]
+
+        let cinema = req.params.filmId;
+        for (let i = 0; i <= film.length; i++) {
+            if (cinema == film[i].id) {
+                return film[i]
+            } else if (cinema !== film[i].id) {
+                return 'No movie exist with this id.'
+            }
+        }
+    }
+    res.send(printMovies())
+
 });
 
 
